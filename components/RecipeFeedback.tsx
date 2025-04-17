dec06fb91a21b4ca6373d3d418f932592770734c
Jan Devrim <jandevrim@gmail.com>
2025-04-17 14:42:19 +0300

    cihaz seçim kaydı

--- components/RecipeFeedback.tsx
+++ components/RecipeFeedback.tsx
@@ -16,7 +16,7 @@ const RecipeFeedback: React.FC<RecipeFeedbackProps> = ({
   recipeText,
   ingredients,
   steps,
-  cihazMarkasi="tumu",
+  cihazMarkasi={cihazMarkasi},
   tarifDili = "tr",
   kullaniciTarifi = false,
 }) => {
