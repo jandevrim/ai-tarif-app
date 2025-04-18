0c5aef283134d9345aef6b43138fffbfdfff68a0
Jan Devrim <jandevrim@gmail.com>
2025-04-18 14:24:05 +0300

    cssfix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"; // ✅ bunu ekle
-
+import '../styles/theme.css'; // Import the global CSS here
 import type { AppProps } from "next/app";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
